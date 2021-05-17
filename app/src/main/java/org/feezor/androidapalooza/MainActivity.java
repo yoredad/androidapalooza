79f3a5cc1ca7691f9bbf644ef3f7e5b398866d0b
YoreDad <feezjaglg@gmail.com>
2021-05-17 00:25:29 -0400

    add inputs and button handling. added input style.

--- app/src/main/java/org/feezor/androidapalooza/MainActivity.java
+++ app/src/main/java/org/feezor/androidapalooza/MainActivity.java
@@ -3,6 +3,9 @@ package org.feezor.androidapalooza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /** Called when the user taps the Send button */
+    public void sendMessage( View view) {
+        // Do something in response to button
+        EditText editText = (EditText) findViewById(R.id.textBox);
+        String inp = editText.getText().toString().trim();
+        TextView tf = (TextView) findViewById(R.id.textView3);
+        tf.setText(inp);
+    }
+
 }
