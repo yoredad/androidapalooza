0b8812c086a28c572de9148c2899eeaba110ec7d
YoreDad <feezjaglg@gmail.com>
2021-05-17 01:36:31 -0400

    fixed layout issues with keyboard

--- app/src/main/java/org/feezor/androidapalooza/MainActivity.java
+++ app/src/main/java/org/feezor/androidapalooza/MainActivity.java
@@ -2,8 +2,10 @@ package org.feezor.androidapalooza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -21,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
         String inp = editText.getText().toString().trim();
         TextView tf = (TextView) findViewById(R.id.textView3);
         tf.setText(inp);
+        hideKeyboard(this);
     }
 
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
 }
